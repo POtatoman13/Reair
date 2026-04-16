a2c8ab29787ce34b1f3f4b1d6f32739c3bb31529
POtatoman13 <laspisachristian7@gmail.com>
2026-04-16 20:58:28 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Importante per caricare Tailwind CSS
 
+// Inizializza l'applicazione React nell'elemento 'root' del file index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
